1d8dddb5f68b4c6bb3f21c59296c459a3b94cffb
Mehrab Rami <mehrabrami8@gmail.com>
2026-09-19 02:19:08 +0600

    Add initial implementation of student data program

---
+++ data types/Data types.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int studentID = 2025331539;
+    float score = 3.45;
+    char grade = 'A';
+
+    printf("StudentId is %d", studentID);
+    printf("\nScore is %.2f", score);
+    printf("\ngrade is %c", grade);
+    return 0;
+}
