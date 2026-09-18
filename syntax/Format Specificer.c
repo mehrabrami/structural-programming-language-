f27b7c5bcd823dc566e9c0f5c5a48d035747458a
Mehrab Rami <mehrabrami8@gmail.com>
2026-09-19 02:44:07 +0600

    Create Format Specificer.c

---
+++ syntax/Format Specificer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int i; 
+	char a;
+	float f;
+	double dd;  // variables
+	
+	printf("Enter values for int character floating and doubles : \n");
+	scanf("%d %f %c %lld"); // this are format specifiers 
+	
+	printf("You have entered int %d \n char %c \n floating number %f \n double number %lld ", i,a,f,dd);
+	
+	
+
+}
