f5a3e1831da5adfa6ac280135f96c88cd6da30e0
Mehrab Rami <mehrabrami8@gmail.com>
2026-09-19 02:28:45 +0600

    Create while.c

---
+++ while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Declaring i and   initialising 
+    int i=1;
+    // using while loop
+    while (i <= 5) {
+        printf("%d ", i);
+        i++; // increment
+    }
+    return 0;
+}
