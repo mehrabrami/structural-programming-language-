3fcf41d81c2f524f8c32e4d401d9b1c9af8b929e
Mehrab Rami <mehrabrami8@gmail.com>
2026-09-19 02:30:21 +0600

    Add Assignment Operators demonstration in C

---
+++ operators/Assignment.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	// Assignment Operators
+	int p=10;
+	
+	printf("p = %d \n", p);
+	printf("p += 5 : %d\n",p+=5);
+	printf("p -= 5 : %d\n",p-=5);
+	printf("p *= 5 : %d\n",p*=5);
+	printf("p /= 5 : %d\n",p/=5);
+	printf("p %= 5 : %d\n",p%=5);
+	
+return 0;
+}
