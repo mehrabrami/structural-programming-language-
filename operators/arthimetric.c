7fcbb53c475ed5bafb2f2f09a6b7b9d706d4a9cf
Mehrab Rami <mehrabrami8@gmail.com>
2026-09-19 02:31:32 +0600

    Add arithmetic operations in arthimetric.c

---
+++ operators/arthimetric.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int a=10 , b=5;
+	
+	printf("Plus : %d ",a+b);
+	printf("Minus : %d ", a-b);
+	printf("Multipication : %d ", a*b);
+	printf("Division : %d ", a/b);
+	printf("Mod : %d ", a%b);
+	return 0;
+
+}
