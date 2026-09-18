db22df6683ea8c4d403b1ada197fa1adceba6ba6
Mehrab Rami <mehrabrami8@gmail.com>
2026-09-19 02:43:17 +0600

    Create Comments.c

---
+++ syntax/Comments.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	// my code goes here
+	int i,n1,n2;
+	printf("Enter two numbers : \n"); //displays for asking two numbers
+	scanf("%d %d ", &n1,&n2);
+	//taking input and storing in variable n1 and n2
+	
+	printf("You have entered : %d %d ", n1,n2);
+	//Printing inputed value by user
+	return 0;
+
+}
